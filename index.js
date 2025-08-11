f106f4a82ce1cee4b9660fef512fbca3f3e76f30
Yasin ALAN <yasinalan9963@gmail.com>
2025-08-11 16:10:20 +0300

    Added mongoDB connection

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 const todoRoutes = require('./src/routes/todo.routes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('/api', todoRoutes);
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB bağlantısı başarılı'))
+  .catch((err) => console.error('MongoDB bağlantı hatası:', err));
+
 app.get('/', (req, res) => {
   res.send('Merhaba! To-Do API çalışıyor.');
 });
 
-app.get 
-
 app.listen(PORT, () => {
-    console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor...`);
+  console.log(`Sunucu http://localhost:${PORT} adresinde çalışıyor...`);
 });
